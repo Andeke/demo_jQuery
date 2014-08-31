e2489272dadcb4632649ccd545b5149c9129842b
Andreas <andeke0718@gmail.com>
2014-08-31 03:59:49 +0200

    Removed useless lines; added flashy animation
    Addded an animation for you. Don't think I'd do this for anyone!

--- script.js
+++ script.js
@@ -2,27 +2,59 @@ $(document).ready(function() {
 
     // Reaktion av tryck på länk
     $("#add_item").click(function(event) {
-        // så att sidan ej laddas om
         event.preventDefault();
 
-        // Hämta värdena ut formuläret
+        // Hämta värdena ur formuläret
         var first_name  = $("#first_name").val();
         var last_name   = $("#last_name").val();
         var age         = $("#age").val();
+        var country     = $("#country").val();
         var profession  = $("#profession").val();
 
-        // Lägg till element
-        if(first_name !== "" && last_name !== "" && age !== "" && profession !== "")
+        // Lägg till element i lista om formuläret innehåller inmatad data
+        if(first_name !== "" && last_name !== "" && age !== "" && country !== "" && profession !== "")
         {
-            var item = "<div class=\"item\">" + first_name + " " + last_name + "</div>";
+            // var item = "<div class=\"item\">" + first_name + " " + last_name + "</div>";
+
+            var item = "<div class=\"item\">" +
+                            "<div class=\"first_row\">" +
+                               "<span class=\"span_first_name\">" + first_name + "</span> " +
+                               "<span class=\"span_last_name\">" + last_name + "</span>" +
+                               "<a class=\"item_remove\" href=\"#\">Remove</a>"+
+                               "<a class=\"item_edit\" href=\"#\">Edit</a>"+
+                               "<a class=\"item_details\" href=\"#\">Details</a>" +
+                            "</div>"+
+                            "<div class=\"item_age_profession\">Age: <span class=\"span_age\">"+age+
+                                "</span> Country: <span class=\"span_country\">"+country+
+                                "</span> Profession: <span class=\"span_profession\">"+profession+"</span></div>"+
+                       "</div>";
 
             $("#list_of_items").append(item);
 
-            // Töm formuläret
+            // Töm formuläret på inmatad data
             $("#item_form").trigger("reset");
 
-            // Sätt fokus i formuläret
+            // Sätt fokus i formuläret efter att data har inmatats
             $("#first_name").focus();
+        }
+    });
+
+    // Reagera på klickning av detalj-knappen
+    $("#list_of_items").on("click", ".item_details", function(event) {
+        event.preventDefault();
+
+        if($(this).parent().parent().children(".item_age_profession").is(":hidden"))
+        {
+            // Visa detaljer
+            $(this).parent().parent().children(".item_age_profession").slideDown("slow")
+        }
+        else
+        {
+            // Göm detaljer
+            $(this).parent().parent().children(".item_age_profession").slideUp("slow", function() {
+                $(this).parent().parent().children(".item_age_profession").hide();
+            });
+
         }
     });
 
